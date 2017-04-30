3338c1fd8b1f723fc814fe9aca50ae734ce6ab7c
Aashish Panvelkar <Aashish Panvelkar>
2017-04-30 11:52:30 -0400

    Updating

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,42 +1,102 @@
-$(document).ready(function() {
-
-var questions = ["I like watching TV Shows", "I am a movie buff", "I play lots of sports", "I like playing video games",
-                    "I own a pet", "I like to listen to music", "I am workoholic", "I am an outgoing person", "I like to read books",
-                    "I love food from different culture"];
-
-var userdata = {};
-
-var populateSurvey = function()
+function doSubmit()
 {
-     
-    for (i = 0; i< questions.length; i++)
-    {
     
-       var myDropDown = '<div class="dropdown">'+
-    '<button class="btn btn-primary dropdown-toggle" id="menu'+i+'"'+' type="button" data-toggle="dropdown">Choices'+
-    '<span class="caret"></span></button>'+
-    '<ul class="dropdown-menu" role="menu" aria-labelledby="menu1">'+
-      '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">1 [Strongly Disagree]</a></li>'+
-      '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">2</a></li>'+
-      '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">3</a></li>'+
-      '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">4</a></li>'+  
-      '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">5 [Strongly Agree]</a></li>'+   
-        '</ul>'+
-            '</div>';
-        var survey = questions[i];
+    // var buttonData = $('#menu'+i).val();
         
-        
-        $('#questions').append('<h3>'+survey+'</h3>');
-        
-        $('#questions').append(myDropDown);
-        $('#questions').append('-----------------------');
-        
-        $('#questions').append('<p>');
-       
+        // score.push(buttonData);
+
+    console.log("This is submit button!");
+    userdata.name   =   $('#nameEntry').val().trim();
+    userdata.photo  =   $('#imageEntry').val().trim();
+    // userdata.score = 
+    // userdata.scores =   $('#imageEntry').val().trim();
+    // console.log(input);
+    console.log(userdata);
+
+    
+        // console.log(buttonData);
+
+        console.log(window.scoreData);
+    
+}
+
+
+// function setValue(  )
+// {
+//         var selText = $(this).text();
+
+//             console.log(selText);
+//             alert("Value: " +selText);
+//             scoreData.menu0 = selText;
+//             alert("Value2: " +scoreData.menu0);
+//             // var scoreValue = selText;
+
+//             // alert("alert: " + selText);
+//             // console.log(scoreValue);
+
+            
+//     }
+
+    window.scoreData = {};
+
+    var userdata = {};
+
+$(document).ready(function() 
+{
+
+//I was thinking about automating the questions and using event handlers for storing in to object but ran in to issues of scoping. 
+
+// var questions = ["I like watching TV Shows", "I am a movie buff", "I play lots of sports", "I like playing video games",
+//                     "I own a pet", "I like to listen to music", "I am workoholic", "I am an outgoing person", "I like to read books",
+//                     "I love food from different culture"];
+
+//scoreData = [];
+
+
+$('#submitButton').on("click", function() 
+{
+
+
+    console.log("This is submit button!");
+    
+    
+    // console.log(userdata);
+    // console.log($('#questOne').val());
+
+    if(!$('#nameEntry').val() || !$('#imageEntry').val())
+    {
+
+    
+    console.log("Name or Image text is empty!");
+
+    
+    }else{
+
+        var userdata = 
+        {
+            name: $('#nameEntry').val(),
+            image: $('#imageEntry').val(),
+            score: 
+            [
+                $('#questOne').val(),
+                $('#questTwo').val(),
+                $('#questThree').val(),
+                $('#questFour').val(),
+                $('#questFive').val(),
+                $('#questSix').val(),
+                $('#questSeven').val(),
+                $('#questEight').val(),
+                $('#questNine').val(),
+                $('#questTen').val()
+                
+            ]
+        };
+    console.log(userdata);
     }
 
-}
-populateSurvey();
 
+    
+        
+});
 
 });
